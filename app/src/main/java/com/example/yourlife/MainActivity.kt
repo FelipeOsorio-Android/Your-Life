710e6ba379044183454cf0a871b48f6c0401c56d
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2024-06-29 23:26:44 -0300

    implement singleton dataStore

--- app/src/main/java/com/example/yourlife/MainActivity.kt
+++ app/src/main/java/com/example/yourlife/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.yourlife
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.doublePreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.yourlife.databinding.ActivityMainBinding
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "userAttributes")
+
+sealed class PreferenceKey {
+
+    companion object {
+        val userImc = doublePreferencesKey("imc")
+    }
+}
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
     }
 }
