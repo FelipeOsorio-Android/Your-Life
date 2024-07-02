e9085b31a1b61a4b9acac5cd1952095984a22579
FelipeOsorio-Android <feeh_devandroid@hotmail.com>
2024-07-01 22:52:11 -0300

    refactoring logic

--- app/src/main/java/com/example/yourlife/fragments/HomeScreenFragment.kt
+++ app/src/main/java/com/example/yourlife/fragments/HomeScreenFragment.kt
@@ -1,21 +1,19 @@
 package com.example.yourlife.fragments
 
 import android.os.Bundle
+import android.text.Layout.Directions
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.core.widget.doOnTextChanged
-import androidx.datastore.preferences.core.edit
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.yourlife.PreferenceKey
 import com.example.yourlife.R
-import com.example.yourlife.dataStore
 import com.example.yourlife.databinding.FragmentHomeBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.math.BigDecimal
+import java.text.DecimalFormat
+import java.text.DecimalFormatSymbols
+import java.util.Locale
 import kotlin.math.pow
 
 class HomeScreenFragment : Fragment() {
@@ -46,51 +44,55 @@ class HomeScreenFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        btnCalculate.isEnabled = false
+        animation.setMaxProgress(0.65F)
+        animation.playAnimation()
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        btnCalculate.isEnabled = false
-        animation.setMaxProgress(0.65F)
-        animation.playAnimation()
+        editHeight.editText?.doOnTextChanged { _, _, _, _ ->  enableButton() }
 
-        var altura = ""
-        var peso = ""
-
-        editHeight.editText?.doOnTextChanged { text, _, _, _ ->
-            text?.let {
-                altura = it.toString()
-            }
-
-            btnCalculate.isEnabled = altura.isNotEmpty() && peso.isNotEmpty()
-        }
-
-        editWeight.editText?.doOnTextChanged { text, _, _, _ ->
-            text?.let {
-                peso = it.toString()
-            }
-
-            btnCalculate.isEnabled = altura.isNotEmpty() && peso.isNotEmpty()
-        }
-
+        editWeight.editText?.doOnTextChanged { _, _, _, _ -> enableButton() }
 
         btnCalculate.setOnClickListener {
 
-            val userImc = peso.toInt()/altura.toDouble().pow(2)
+            val bundle = bundleOf("userImc" to calculateImc())
 
-            CoroutineScope(Dispatchers.IO).launch {
-                saveUserImc(userImc)
-            }
-
-            findNavController().navigate(R.id.action_homeScreenFragment_to_resultScreenFragment)
+            findNavController().navigate(
+                R.id.action_homeScreenFragment_to_resultScreenFragment,
+                args = bundle
+            )
         }
     }
 
-    private suspend fun saveUserImc(userImc: Double) {
-        binding.root.context.dataStore.edit {
-            it[PreferenceKey.userImc] = userImc
-        }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun enableButton() {
+
+        val altura = editHeight.editText?.text.toString()
+        val peso = editWeight.editText?.text.toString()
+
+        btnCalculate.isEnabled = altura.isNotEmpty() && peso.isNotEmpty()
+    }
+
+    private fun calculateImc(): String {
+
+        val altura = editHeight.editText?.text.toString().toDouble()
+        val peso = editWeight.editText?.text.toString().toInt()
+
+        val userImc = peso/altura.pow(2)
+
+        val formatter = DecimalFormat("#00.00",DecimalFormatSymbols.getInstance(Locale("pt", "br")))
+
+        val userImcFormatter = formatter.format(userImc)
+
+        return userImcFormatter
     }
 }
